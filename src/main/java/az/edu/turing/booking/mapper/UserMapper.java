abdb24dcf13e9ff0b678bfd55feba57d22605ca3
Behruz Tapdiqov <tapdigovbahruz@gmail.com>
2025-01-30 20:15:01 +0400

    AdminUser updated completed

--- src/main/java/az/edu/turing/booking/mapper/UserMapper.java
+++ src/main/java/az/edu/turing/booking/mapper/UserMapper.java
@@ -1,12 +1,14 @@
 package az.edu.turing.booking.mapper;
 
 import az.edu.turing.booking.domain.entity.UserEntity;
+import az.edu.turing.booking.model.dto.request.UpdateUserDto;
 import az.edu.turing.booking.model.dto.response.UserDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
     UserDto toUserDto(UserEntity userEntity);
+
+    UpdateUserDto toDto(UserEntity userEntity);
 
 }
--- src/main/java/az/edu/turing/booking/service/admin/AdminUserService.java
+++ src/main/java/az/edu/turing/booking/service/admin/AdminUserService.java
@@ -4,6 +4,7 @@ import az.edu.turing.booking.domain.entity.UserEntity;
 import az.edu.turing.booking.domain.repository.UserRepository;
 import az.edu.turing.booking.exception.NotFoundException;
 import az.edu.turing.booking.mapper.UserMapper;
+import az.edu.turing.booking.model.dto.request.UpdateUserDto;
 import az.edu.turing.booking.model.dto.response.UserDto;
 import az.edu.turing.booking.model.enums.UserStatus;
 import lombok.RequiredArgsConstructor;
@@ -36,5 +37,21 @@ public class AdminUserService {
         UserEntity userEntity = repository.findById(id)
                 .orElseThrow(() -> new NotFoundException("User with specified id not found"));
         return userMapper.toUserDto(userEntity);
+    }
+
+    @Transactional
+    public UserDto update(long id, UpdateUserDto updatedUserDto) {
+        UserEntity user = repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("User with specified id not found"));
+        user.setFirstName(updatedUserDto.getFirstName());
+        user.setLastName(updatedUserDto.getLastName());
+        user.setEmail(updatedUserDto.getEmail());
+        user.setPhoneNumber(updatedUserDto.getPhoneNumber());
+        user.setDateOfBirth(updatedUserDto.getDateOfBirth());
+        user.setNationality(updatedUserDto.getNationality());
+        user.setRole(updatedUserDto.getRole());
+        user.setStatus(updatedUserDto.getStatus());
+        UserEntity updatedUserEntity = repository.save(user);
+        return userMapper.toUserDto(updatedUserEntity);
     }
 }
