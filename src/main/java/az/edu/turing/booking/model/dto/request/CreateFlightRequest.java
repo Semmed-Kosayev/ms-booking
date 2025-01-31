10f4c571467db4ec45fb64b7470942fb2b3df143
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2025-01-31 08:05:19 +0400

    amended dtos: changed data types and added validations

--- src/main/java/az/edu/turing/booking/model/dto/request/CreateFlightRequest.java
+++ src/main/java/az/edu/turing/booking/model/dto/request/CreateFlightRequest.java
@@ -1,8 +1,13 @@
 package az.edu.turing.booking.model.dto.request;
 
+import az.edu.turing.booking.model.enums.AircraftModel;
+import az.edu.turing.booking.model.enums.City;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -10,20 +15,37 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateFlightRequest {
+
+    @NotBlank
     private String airlineName;
+    @NotNull @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime departureTime;
+    @NotNull @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime arrivalTime;
+    @NotBlank
     private String departureAirport;
+    @NotBlank
     private String arrivalAirport;
-    private String departureCity;
-    private String arrivalCity;
-    private String aircraftModel;
-    private String departureTerminal;
+    @NotNull
+    private City departureCity;
+    @NotNull
+    private City arrivalCity;
+    @NotNull
+    private AircraftModel aircraftModel;
+    @NotNull
+    private AircraftModel departureTerminal;
+    @NotBlank
     private String arrivalTerminal;
+    @NotNull
     private Integer gateNumber;
+    @NotNull
     private Integer maxBaggageWeight;
+    @NotNull
     private Boolean isWifiAvailable;
+    @NotNull
     private Integer availableSeats;
+    @NotNull
     private Integer maxSeats;
+    @NotBlank
     private String flightStatus;
 }
--- src/main/java/az/edu/turing/booking/model/dto/request/UpdateUserDto.java
+++ src/main/java/az/edu/turing/booking/model/dto/request/UpdateUserDto.java
@@ -3,6 +3,9 @@ package az.edu.turing.booking.model.dto.request;
 import az.edu.turing.booking.model.enums.Nationality;
 import az.edu.turing.booking.model.enums.Role;
 import az.edu.turing.booking.model.enums.UserStatus;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,12 +18,21 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Builder
 public class UpdateUserDto {
+    @NotBlank
     private String firstName;
+    @NotBlank
     private String lastName;
+    @NotBlank
+    @Email
     private String email;
+    @NotBlank
     private String phoneNumber;
+    @NotNull
     private LocalDate dateOfBirth;
+    @NotNull
     private Nationality nationality;
+    @NotNull
     private Role role;
+    @NotNull
     private UserStatus status;
 }
