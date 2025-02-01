ae9d6218b2f5867ca095fd198fecc2a01e63b659
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2025-02-01 22:10:15 +0400

    changed return type to ResponseEntity<Void> in delete method of controllers

--- src/main/java/az/edu/turing/booking/controller/BookingController.java
+++ src/main/java/az/edu/turing/booking/controller/BookingController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -49,9 +48,9 @@ public class BookingController {
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@Min(1) @NotNull @PathVariable Long id) {
+    public ResponseEntity<Void> delete(@Min(1) @NotNull @PathVariable Long id) {
         service.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
     @PostMapping
--- src/main/java/az/edu/turing/booking/controller/admin/AdminBookingController.java
+++ src/main/java/az/edu/turing/booking/controller/admin/AdminBookingController.java
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 @RequestMapping("/api/v1/admin/bookings")
 public class AdminBookingController {
 
