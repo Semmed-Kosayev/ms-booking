7419aca8241e609bb7ff707930b6103d383c1832
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2025-02-06 19:58:16 +0400

    solved n+1 problem

--- src/main/java/az/edu/turing/booking/domain/repository/FlightRepository.java
+++ src/main/java/az/edu/turing/booking/domain/repository/FlightRepository.java
@@ -1,6 +1,10 @@
 package az.edu.turing.booking.domain.repository;
 
 import az.edu.turing.booking.domain.entity.FlightEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -9,6 +13,9 @@ import java.time.LocalDateTime;
 
 @Repository
 public interface FlightRepository extends JpaRepository<FlightEntity, Long>, JpaSpecificationExecutor<FlightEntity> {
+
+    @EntityGraph(attributePaths = {"flightDetail"})
+    Page<FlightEntity> findAll(Specification<FlightEntity> spec, Pageable pageable);
 
     boolean existsByDepartureTimeAndDepartureAirportAndArrivalAirportAndAirlineName
             (LocalDateTime departureTime, String departureAirport, String arrivalAirport, String airlineName);
