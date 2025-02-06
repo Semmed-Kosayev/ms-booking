69b7c1ff1517c6054214940180ff9baeb859cb3d
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2025-02-06 20:05:34 +0400

    solved n+1 problem (#30)

--- src/main/java/az/edu/turing/booking/domain/repository/BookingRepository.java
+++ src/main/java/az/edu/turing/booking/domain/repository/BookingRepository.java
@@ -3,10 +3,17 @@ package az.edu.turing.booking.domain.repository;
 import az.edu.turing.booking.domain.entity.BookingEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookingRepository extends JpaRepository<BookingEntity, Long> {
+
+    @EntityGraph(attributePaths = {"passenger", "flight"})
+    List<BookingEntity> findAll();
+
     Page<BookingEntity> findAllByPassengerId(Long passengerId, Pageable pageable);
 }
